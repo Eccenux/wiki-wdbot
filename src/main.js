f6c2c4cc38687c29a268056ce6105356886ee4c2
eccenux <eccenux+git@gmail.com>
2023-09-11 21:21:59 +0200

    Mass-remove a property

--- src/main.js
+++ src/main.js
@@ -1,6 +1,36 @@
+/* global WikidataApiHandler */
 // Test
+/**
 var wikidataApi = new WikidataApiHandler();
 var entityId = 'Q30159570';
 var propertyId = 'P625';
 
 await wikidataApi.removeProp(entityId, propertyId);
+/**/
+
+//
+// Q-list from quick statements
+var input = document.querySelector('textarea');
+var qList = input.value
+	.replace(/[\r\n]+/g, '\n')
+	.split('\n')
+	.map(v => v.replace(/^(Q[0-9]+).+/g, '$1'))
+;
+
+//
+// Mass-remove a property
+var wikidataApi = new WikidataApiHandler();
+var propertyId = 'P625';
+run(qList);
+async function run(qList) {
+	console.log(`Running %d removals of ${propertyId}.`, qList.length);
+	let removed = 0;
+	for (const entityId of qList) {
+		let ok = await wikidataApi.removeProp(entityId, propertyId);
+		if (ok) {
+			console.log(`Removed from ${entityId}`);
+			removed++;
+		}
+	}
+	console.log(`Done. Removed %d of %d of ${propertyId}.`, removed, qList.length);
+}
